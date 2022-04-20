3199b31f14908f56c4b1d4a5924a18df5699207d
okulchitska <olyakylchitska@gmail.com>
2022-04-20 07:46:04 -0700

    v3

---
+++ Tests/octane sdk/test (automated_Jenkins)/Res1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\Administrator\\Desktop\\sdk scripts\\DEFAULT_sdk\\Tests\\octane sdk\\test (automated_Jenkins)\\Res1',namePairs:[]}]};
